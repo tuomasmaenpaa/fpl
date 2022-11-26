1bead30f3b5a27c12e136a389b7c8b641026c8dc
Tuomas Mäenpää <tuomas.maenpaa1@gmail.com>
2022-11-26 11:17:16 +0200

    Added a getter for PL fixtures
    Added a function which returns PL fixtures
    as a pandas DataFrame where each team's fixtures
    are in their indivitual columns. The gameweek number
    is the index.

--- PremierLeague.py
+++ PremierLeague.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import json
+import time
 
 TEAM_NAME_CONVERSION = {
     "Arsenal": "Arsenal",
@@ -26,28 +28,32 @@ TEAM_NAME_CONVERSION = {
 }
 
 def fbref_to_fpl(name):
-    return TEAM_NAME_CONVERSION[name]
+    try:
+        return TEAM_NAME_CONVERSION[name]
+    except KeyError:
+        return name
 
 def parse_team_name(url):
     team = (' ').join(url.split('/')[-1].split('-')[:-1]).replace('-', ' ')
     return team
 
 def get_team_urls():
-    standing_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+    standing_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
     data = requests.get(standing_url)
-    soup = BeautifulSoup(data.text)
-    standing_table = soup.select("table.stats_table")[0]
+    soup = BeautifulSoup(data.text, features='lxml')
+    standing_table = soup.select('table.stats_table')[0]
     
-    links  = standing_table.find_all("a")
-    links = [l.get("href") for l in links]
+    links  = standing_table.find_all('a')
+    links = [l.get('href') for l in links]
     links = [l for l in links if '/squads/' in l]
     
-    team_urls = [f"https://fbref.com{l}" for l in links]
-    return team_urls
+    team_urls = [f'https://fbref.com{l}' for l in links]
+    team_names = [parse_team_name(url) for url in team_urls] 
+    return list(zip(team_names, team_urls))
     
 def get_league_table():
     
-    standing_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+    standing_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
     data = pd.read_html(standing_url, match='Regular season Table')
     
     table = data[0]
@@ -58,5 +64,20 @@ def get_league_table():
 
 def get_fixture_table():
     
+    fixtures = {
+        'Gameweek': [gw for gw in range(1,39)]
+    }
+    
     team_urls = get_team_urls()
+    team_urls.sort(key=lambda t: t[0])
+    for name, url in team_urls:
+        data = pd.read_html(url, match = 'Scores & Fixtures')[0]
+        data = data[data.Comp == 'Premier League']
+        data['Opponent'] = data['Opponent'].apply(lambda x: fbref_to_fpl(x))
+        
+        fixtures[fbref_to_fpl(name)]= data[['Opponent', 'Venue']].agg('-'.join, axis=1).tolist()
+        # Sleep 8s to keep the request rate below fbref limit
+        time.sleep(8)
+        
+    return pd.DataFrame(fixtures).set_index('Gameweek')
 
